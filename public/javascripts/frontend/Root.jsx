88a7c0de61a92a456de9cb0604cff63869a569c4
Colin Ritchey <colinritchey256@gmail.com>
2017-05-03 17:57:51 -0700

    expanded react component, added isometric fetch npm package, currently fetches repos

--- public/javascripts/frontend/Root.jsx
+++ public/javascripts/frontend/Root.jsx
@@ -3,22 +3,53 @@ import React from 'react';
 class Root extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {user: '', data: ''};
+    this.state = {username: 'colinritchey'};
+
+    this.update = this.update.bind(this);
   }
 
   componentDidMount(){
 
     $.ajax({
-      url: 'api/users'
-    }).then(res => console.log( this.setState({user: res.login}) ));
+      url: 'api/users',
+      data: { gitInfo: this.state }
+    }).then(res => console.log( this.setState({username: res.login}) ));
     // debugger;
 
+  }
+
+  update(field){
+    return e => this.setState({[field]: e.target.value});
+  }
+
+  handleSubmit(){
+    console.log(this.state.user);
+
+    // $.ajax({
+    //   url: 'api/users'
+    // }).then(res => console.log( this.setState({user: res.login}) ));
   }
 
   render() {
     return (
       <div>
-        <h1>Hello {this.state.user}</h1>
+        <h1>Hello {this.state.username}</h1>
+        <form onSubmit={() => this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={this.update("username")}
+            >
+          </input>
+
+          <input
+            type="submit"
+
+            >
+
+          </input>
+
+        </form>
       </div>
     );
   }
--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,21 @@ const request = require('request');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.setHeader('Content-Type', 'application/json');
+  // res.setHeader('Content-Type', 'application/json');
+  // console.log(res, "res");
+  console.log(req.params.username, "req params");
 
+  let username = req.query.gitInfo.username;
+  let data;
+  console.log(username, "req query");
   request({
-    url: 'https://api.github.com/users/colinritchey',
+    url: `https://api.github.com/users/${username}/repos`,
     headers: {'User-Agent': 'sampleApp'}
   }).pipe(res);
+
+  // console.log(res, "res");
+  // console.log(data, "data");
+  // ;
 
 });
 
